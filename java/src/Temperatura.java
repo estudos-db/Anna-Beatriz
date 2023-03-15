1a3322059c981048d25e286d9739050e68bb0299
annablsantos <annabea608@gmail.com>
2023-03-15 14:45:27 -0300

    adicionei fórmula para a conversao

--- java/src/Temperatura.java
+++ java/src/Temperatura.java
@@ -7,12 +7,18 @@ public class Temperatura {
         double c,f; //Permite a entrada de dados via teclado quando usamos o console
         //Objetos
         Scanner teclado = new Scanner(System.in);
+
         DecimalFormat formatador = new DecimalFormat("#0.0"); //Formata o resultado em apenas uma casa decimal
+
         System.out.println("Conversão de temperatura");
-        System.out.print("Digite a temperatura em fahreinheit!");
+        System.out.print("Digite a temperatura em fahreinheit para conversão:");
         //Detectar o que foi digitado no teclado e armazenar na variável f
         f = teclado.nextDouble();
         //processamento
-        c = (5 * f )
+        c = (5 * (f - 32)) / 9;
+        //exibir resultado
+        System.out.println("A temperatura em celsius é:" + formatador.format(c) + "ºC");
+
+        teclado.close(); // Não permite mais entrada de dados.
     }
 }
