35cb7c4339f7716d6ceb3ca2fd2c30c9267e13a7
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-03-15 17:12:08 -0300

    Add files via upload

---
+++ LimiteDeCompras.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class LimiteDeCompras {
+    public static void main(String[] args) {
+        double limite = 100.0;
+        List<Double> compras = new ArrayList<>();
+        compras.add(5.0);
+        compras.add(40.0);
+        compras.add(50.0);
+        //Totaliza 95 e o limite do usuário é 100, então o limite não foi excedido
+
+        boolean limiteExcedido = LimiteDeCompras.seLimiteForExcedido(limite, compras);
+        if (limiteExcedido) {
+            System.out.println("Limite de compras foi excedido.");
+        } else {
+            System.out.println("Seu limite de compras não foi excedido.");
+        }
+    }
+    public static boolean seLimiteForExcedido(double limit, List<Double> compras) {
+        double total = 0;
+        for (double compra : compras) {
+            total += compra;
+        }
+        return total > limit;
+    }
+}
