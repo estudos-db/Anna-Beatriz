21222d9a29e7c8a50fe48f4c7b2eb587fbe3ee28
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-03-30 13:36:08 -0300

    alteraçao do nome das variáveis

--- ContarFibonacciAteCertoNumero.java
+++ ContarFibonacciAteCertoNumero.java
@@ -4,17 +4,17 @@ public class ContarFibonacciAteCertoNumero {
         Scanner input = new Scanner(System.in);
         System.out.print("Insira um número inteiro que não seja negativo: ");
 
-        int numero = input.nextInt();
+        int numeroDoUsuario = input.nextInt();
 
-        //primeiros valores da sequencia fibonacci, variavel c armazena soma
-        int a = 0, b = 1, c;
+        //primeiros valores (termos) da sequencia fibonacci e o próximo termo da sequência, após a soma dos dois números anteriores
+        int primeiroTermo = 0, segundoTermo = 1, termoPosSoma;
 
-        //loop que roda ate que o valor de "a" seja maior que o numero inserido pelo usuario
-        while (a <= numero) {
-            System.out.print(a + ",");
-            c = a + b;
-            a = b;
-            b = c;
+        //loop que roda ate que o valor do primeiro termo seja maior que o numero inserido pelo usuario
+        while (primeiroTermo <= numeroDoUsuario) {
+            System.out.print(primeiroTermo + ",");
+            termoPosSoma = primeiroTermo + segundoTermo;
+            primeiroTermo = segundoTermo;
+            segundoTermo = termoPosSoma;
         }
 
         System.out.println();
