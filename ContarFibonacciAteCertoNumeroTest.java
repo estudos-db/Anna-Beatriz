4ead9352453c074bea4dcb8c945b8703fa62f89d
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-03-31 15:15:11 -0300

    refatoração pós feedback

--- ContarFibonacciAteCertoNumeroTest.java
+++ ContarFibonacciAteCertoNumeroTest.java
@@ -1,25 +1,23 @@
 package testjava;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 public class ContarFibonacciAteCertoNumeroTest {
+//testa sequencias de entrada
     @Test
-    public void testContarFibonacciAteCertoNumero() {
-        //sequência gerada pra alguns valores de entrada
-        assertEquals("0,1,1,2,3,5,8,", sequenciaFibonacci(10));
-        assertEquals("0,1,1,2,3,5,8,13,21,34,", sequenciaFibonacci(35));
-        assertEquals("0,1,1,2,3,", sequenciaFibonacci(3));
-    }
-    private String sequenciaFibonacci(int numero) {
-        int a = 0, b = 1, c;
-        String resultado = "";
+    public void testSequenciaFibonacci() {
 
-        while (a <= numero) {
-            resultado += a + ",";
-            c = a + b;
-            a = b;
-            b = c;
-        }
-        return resultado;
+        String resultadoEsperado1 = "0,1,1,2,3,5,";
+        String resultadoObtido1 = ContarFibonacciAteCertoNumero.sequenciaFibonacci(5);
+        Assertions.assertEquals(resultadoEsperado1, resultadoObtido1);
+
+        String resultadoEsperado2 = "0,1,1,2,3,5,8,";
+        String resultadoObtido2 = ContarFibonacciAteCertoNumero.sequenciaFibonacci(10);
+        Assertions.assertEquals(resultadoEsperado2, resultadoObtido2);
+
+        //testa entrada negativa
+        String resultadoEsperado3 = "";
+        String resultadoObtido3 = ContarFibonacciAteCertoNumero.sequenciaFibonacci(-1);
+        Assertions.assertEquals(resultadoEsperado3, resultadoObtido3);
     }
 }
