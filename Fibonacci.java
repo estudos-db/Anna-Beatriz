f5975eb9e132be21a4eacf0c79787723c9b010af
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-04-05 18:03:27 -0300

    atualizaçao da logica do exercicio 7

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,26 +1,26 @@
+package testjava;
+
 import java.util.Scanner;
 
 public class Fibonacci {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        System.out.print("Digite um número inteiro que não seja negativo: ");
-        int numero = input.nextInt();
+    public static List<Integer> gerarSequencia(int numero) {
+        List<Integer> sequencia = new ArrayList<>();
 
         //primeiros valores da sequencia fibonacci, variavel c armazena soma
-        int a = 0, b = 1, c;
+        int numeroAnterior = 0, numeroAtual = 1, soma;
 
-        System.out.print(a + ", " + b);
+        sequencia.add(numeroAnterior);
+        sequencia.add(numeroAtual);
 
         //a cada iteraçao, variavel c é atualizada
         for (int i = 1; i < numero; i++) {
-            c = a + b;
-            System.out.print(", " + c);
+            soma = numeroAnterior + numeroAtual;
+            sequencia.add(soma);
 
-        //atualiza os valores anteriores para a proxima iteraçao
-            a = b;
-            b = c;
+            //atualiza os valores anteriores para a proxima iteraçao
+            numeroAnterior = numeroAtual;
+            numeroAtual = soma;
         }
-        System.out.println();
+        return sequencia;
     }
 }
