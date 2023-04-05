ffff6e9b1bb836de137b02be6e5d441ed68eb1ea
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-04-05 18:05:43 -0300

    implementaçao do exercicio 7 com testes

---
+++ Fibonacci.java
@@ -0,0 +1,27 @@
+package testjava;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fibonacci {
+    public static List<Integer> gerarSequencia(int numero) {
+        List<Integer> sequencia = new ArrayList<>();
+
+        //primeiros valores da sequencia fibonacci, variavel c armazena soma
+        int numeroAnterior = 0, numeroAtual = 1, soma;
+
+        sequencia.add(numeroAnterior);
+        sequencia.add(numeroAtual);
+
+        //a cada iteraçao, variavel c é atualizada
+        for (int i = 1; i < numero; i++) {
+            soma = numeroAnterior + numeroAtual;
+            sequencia.add(soma);
+
+            //atualiza os valores anteriores para a proxima iteraçao
+            numeroAnterior = numeroAtual;
+            numeroAtual = soma;
+        }
+        return sequencia;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,9 @@
+package testjava;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> sequencia = Fibonacci.gerarSequencia(9);
+        System.out.println(sequencia);
+    }
+}
