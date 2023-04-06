309ca7bcba2ad0795b25a96583c688c82678f5db
Anna Beatriz <109621978+annablsantos@users.noreply.github.com>
2023-04-06 13:55:43 -0300

    adicionando comentarios no codigo

--- SomaDiagonalPrincipal.java
+++ SomaDiagonalPrincipal.java
@@ -11,9 +11,21 @@ public class SomaDiagonalPrincipal {
     class CalculadoraSoma {
         public static int CalcularSomaDaDiagonalPrincipal(int[][] matriz) {
             int soma = 0;
+            
 
+            /* o loop for percorre todas as linhas da matriz. a variavel "linha" é iniciada com 0
+             porque representa a primeira linha da matriz e enquanto a variável "linha" for menor
+             que o tamanho da matriz (número de linhas da matriz), o loop vai continuar executando.
+             a cada iteraçao desse loop o valor da variavel "linha" é incrementado em 1 e o loop
+             percorre as proximas linhas da matriz.
+             */
             for (int linha = 0; linha < matriz.length; linha++) {
+
+             //armazena a coluna correspondente a linha atual.
                 int coluna = linha;
+                
+             /*adiciona o valor armazenado na posição correspondente à linha e
+                coluna atual da matriz à variável soma.*/
                 soma += matriz[linha][coluna];
             }
             return soma;
