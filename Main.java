1f36cb861fed307e7e64d577f43f2cc6baf654b5
annablsantos <annabea608@gmail.com>
2023-04-13 13:56:22 -0300

    adicionando gradle

--- Main.java
+++
@@ -1,9 +0,0 @@
-package testjava;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) {
-        List<Integer> sequencia = Fibonacci.gerarSequencia(9);
-        System.out.println(sequencia);
-    }
-}
--- src/exercicio7/Fibonacci.java
+++ src/exercicio7/Fibonacci.java
@@ -1,4 +1,4 @@
-package testjava;
+package exercicio7;
 
 import java.util.ArrayList;
 import java.util.List;
