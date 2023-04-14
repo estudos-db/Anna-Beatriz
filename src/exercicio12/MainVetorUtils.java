b7a977db57258f785d78a00cb5e46cadc74a64b4
annablsantos <annabea608@gmail.com>
2023-04-14 15:35:43 -0300

    implementaçao do exercicio 12

---
+++ src/exercicio12/MainVetorUtils.java
@@ -0,0 +1,25 @@
+package src.exercicio12;
+
+import java.util.*;
+
+public class MainVetorUtils {
+
+    public static void main(String[] args) {
+        int[] vetor1 = VetorUtils.criarVetor(50);
+
+        int[] vetor2 = VetorUtils.criarVetor(50);
+
+        System.out.println("Vetor 1 antes da ordenação: " + Arrays.toString(vetor1));
+        BubbleSort.bubbleSort(vetor1);
+        System.out.println("Vetor 1 após a ordenação: " + Arrays.toString(vetor1));
+
+        System.out.println("Vetor 2 antes da ordenação: " + Arrays.toString(vetor2));
+        BubbleSort.bubbleSort(vetor2);
+        System.out.println("Vetor 2 após a ordenação: " + Arrays.toString(vetor2));
+
+        int[] novoVetor = VetorUtils.combinarVetoresOrdenados(vetor1, vetor2);
+
+        System.out.println("Novo vetor após a combinação e ordenação: " + Arrays.toString(novoVetor));
+    }
+}
+
