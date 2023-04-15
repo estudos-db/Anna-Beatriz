f89b2b5ff25ab5ee7535c6045634a0392f56a643
annablsantos <annabea608@gmail.com>
2023-04-15 10:18:02 -0300

    ultima alteraçao

--- src/exercicio13/AlunoMain.java
+++ src/exercicio13/AlunoMain.java
@@ -1,7 +1,4 @@
 package src.exercicio13;
-
-import src.exercicio13.*;
-
 public class AlunoMain {
     public static void main(String[] args) {
         Aluno aluno1 = new Aluno(7.5, 5.0, 3.0, 3.0);
