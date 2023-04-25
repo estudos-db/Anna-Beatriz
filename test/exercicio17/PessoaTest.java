d7870f8bdc528640fb325f4aab0c5441889eba15
annablsantos <annabea608@gmail.com>
2023-04-25 02:10:36 -0300

    refatoraçao

--- test/exercicio17/PessoaTest.java
+++ test/exercicio17/PessoaTest.java
@@ -1,16 +1,16 @@
 package exercicio17;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PessoaTest {
-    Pessoa avoPaterno = new Pessoa("Luiz", 80, null, null);
-    Pessoa avoPaterna = new Pessoa("Ana", 75, null, null);
-    Pessoa pai = new Pessoa("João", 50, avoPaterno, avoPaterna);
-    Pessoa filho = new Pessoa("Pedro", 20, pai, null);
-    Pessoa pessoa = new Pessoa("João", 30, null, null);
+    Pessoa pai = new Pessoa("João", 50);
+    Pessoa mae = new Pessoa("Maria", 45);
+    Pessoa filho = new Pessoa("Pedro", 20, pai, mae);
+    Pessoa pessoa = new Pessoa("João", 30);
     @Test
     @DisplayName("Deve retornar o nome da pessoa.")
     void testGetNome() {
@@ -32,15 +32,15 @@ public class PessoaTest {
     @Test
     @DisplayName("Deve retornar a mãe correta da pessoa.")
     void testGetMae() {
-        Pessoa avoMaterna = new Pessoa("Clara", 80, null, null);
-        Pessoa mae = new Pessoa("Maria", 45, null, avoMaterna);
         assertEquals(mae, filho.getMae());
     }
 
     @Test
     @DisplayName("Deve retornar, em string , a representação da pessoa correta.")
     void testToString() {
-        String expected = "Pessoa{nome='Pedro', idade=20, pai=Pessoa{nome='João', idade=50, pai=Pessoa{nome='Luiz', idade=80, pai=null, mae=null}, mae=Pessoa{nome='Ana', idade=75, pai=null, mae=null}}, mae=null}";
-        assertEquals(expected, filho.toString());
+        Pessoa pessoa = new Pessoa("João", 30);
+        String expected = "Pessoa{nome='João', idade=30}";
+        String actual = pessoa.toString();
+        Assertions.assertEquals(expected, actual);
     }
 }
